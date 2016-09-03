bdda8980816d8748c72aa1baceb77490ee019f3a
Ajinkya Gorad <ajinkyagorad@yahoo.co.in>
2016-09-03 23:33:09 +0530

    modulated signal, sine, square

--- hilbert.m
+++ hilbert.m
@@ -9,32 +9,46 @@ h = 1:n;
 h = 1./h/pi;
 H = [ -fliplr(h) 0 h];   % Generate Filter
 %% Generate Signal
-%square wave
-nCyc = 5;
-f = 100
-oversampling = fs/f
-t = 1/fs:1/fs:nCyc/f;
-maxtime = t(end)
-x = ones(1,oversampling/2);
-X = [];
-for i=1:2*nCyc
-    X= [X ((-1)^i) * x];
-end
-
-% %sine wave
+% %Square wave
+% nCyc = 5;
 % f = 100
 % oversampling = fs/f
-% nCyc = 5;      % cycles of sine wave
+% t = 1/fs:1/fs:nCyc/f;
+% maxtime = t(end)
+% x = ones(1,oversampling/2);
+% X = [];
+% for i=1:2*nCyc
+%     X= [X ((-1)^i) * x];
+% end
+
+% %sine wave
+% f = 1000
+% oversampling = fs/f
+% nCyc = 10;      % cycles of sine wave
 % t = 1/fs:1/fs:nCyc/f;
 % phase = (pi/180)*30; % phase in degrees
-% X = sin(2*pi*f*t+phase);
+% X_sin = sin(2*pi*f*t+phase);
+
+
+
+
 %%Find Transformation
 Xh = conv(X,H); 
 Xh = Xh(n+1:n+length(X));
 
 %% now merge  both signals into one and modulate them
 S = X+j*Xh;
-fc = 40E3  %carrier frequency
+%resample the signal
+S = resample(S,10,1);
+t = resample(t,10,1);
+fc = 30E3  %carrier frequency
+st = S.*exp(j*2*pi*fc*t);
+plot(real(st))
+hold on
+plot(imag(st))
+hold off
+%%now receive the signal
+
 
 
 
